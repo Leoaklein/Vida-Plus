a3fb75b9d66c1634215a271916ccd8bf844b64d6
Leonardo <leoaklein@gmail.com>
2025-11-28 12:33:39 -0300

    add visual das dashboards

--- paciente/urls.py
+++ paciente/urls.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from django.urls import reverse_lazy
 
@@ -8,6 +9,7 @@ app_name = "paciente"
 
 urlpatterns = [
     path("login/", views.login_paciente, name="login"),
+    path('logout/', LogoutView.as_view(next_page='paciente:login'), name='logout'),
     path("dashboard/", views.dashboard_paciente, name="dashboard"),
     path("registrar/", views.registrar_paciente, name="registrar"),
     path("password_reset/", auth_views.PasswordResetView.as_view(template_name="paciente/password_reset_form.html",
@@ -23,5 +25,6 @@ urlpatterns = [
          auth_views.PasswordResetConfirmView.as_view(template_name="paciente/password_reset_confirm.html", success_url = reverse_lazy("paciente:password_reset_complete")),
          name="password_reset_confirm"),
 
-    path("password_reset_complete/", auth_views.PasswordResetCompleteView.as_view(template_name="paciente/password_reset_complete.html"), name="password_reset_complete")
+    path("password_reset_complete/", auth_views.PasswordResetCompleteView.as_view(template_name="paciente/password_reset_complete.html"), name="password_reset_complete"),
+
 ]
